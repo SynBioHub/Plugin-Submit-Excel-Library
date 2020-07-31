66292392b0e017dc9d369c27537d93739f2cf2b4
JMante1 <40006518+JMante1@users.noreply.github.com>
2020-07-31 15:53:01 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+# GSoC2020
+## Spreadsheet Plug-in for SynBioHub
+
+Synthetic Biology is a discipline that connects many disciplines and heterogeneous researchers.
+To be useful as a tool, a platform such as SynBioHub should reflect this diversity. SynBioHub is
+a repository of genetic components and designs and can represent a powerful way to share and
+curate synthetic biology projects. To do so, it should invite and fulfill the needs of
+bioinformaticians and mathematicians while at the same time bridging the gap to wet lab
+experimentalists. One of these differences is the common preference of wet lab biologists to use
+spreadsheets, which as of now cannot be used as input for SynBioHub. This can be changed by
+creating a plugin, which accepts a spreadsheet as input and converts it into SBOL which can be
+processed by SynBioHub. This could simplify the process of data curation for publication by
+bridging the gap between those that perform the experiments and those that write about them
+from the very beginning.
+
+https://medium.com/@IsaMarleen
+
+# Plugin-Submit-Excel
+A very simple submit plugin that returns sbol files with submit information
+
+# Install
+## Docker
+Run `docker run --publish 8093:5000 --detach --name submit-plug synbiohub/plugin-submit-excel:snapshot`
+Check it is up using http://localhost:8093/status.
+
+## Using Python
+Run `pip install -r requirements.tx`t to install the requirements. Then run `FLASK_APP=app python -m flask run`. A flask module will run at http://localhost:5000/.
+
+# Testing
+To create a test manifest use (note that quotes must use " for postman):
+
+```
+run_manifest = {"manifest":[]}
+for i in range(0,7):
+  file_name = f'file_name{i}'
+  file_type = f'file_type{i}'
+  file_url = f'file_url{i}'
+  run_manifest['manifest'].append({"url":file_url, "filename":file_name, "type":file_type})
+```
+Example manifest:
+```
+{"manifest": {"files":[
+  {"url": "file_url0", "filename": "file_name0", "type": "file_type0"},
+  {"url": "file_url1", "filename": "file_name1", "type": "file_type1"},
+  {"url": "file_url2", "filename": "file_name2", "type": "file_type2"},
+  {"url": "file_url3", "filename": "file_name3", "type": "file_type3"},
+  {"url": "file_url4", "filename": "file_name4", "type": "file_type4"},
+  {"url": "file_url5", "filename": "file_name5", "type": "file_type5"},
+  {"url": "file_url6", "filename": "file_name6", "type": "file_type6"}]}}
+  ```
---
+++ app.py
@@ -0,0 +1,136 @@
+from flask import Flask, request, abort, send_file, jsonify
+import os, shutil, glob, random, string
+from Excel import read_library, quality_check, write_sbol
+from sbol2 import *
+#import all functions from .py files
+
+app = Flask(__name__)
+
+@app.route("/status")
+def status():
+    return("The Submit Excel Plugin Flask Server is up and running")
+
+
+
+@app.route("/evaluate", methods=["POST"])
+def evaluate():
+    #uses MIME types
+    #https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+    
+    eval_manifest = request.get_json(force=True)
+    files = eval_manifest['manifest']['files']
+    
+    eval_response_manifest = {"manifest":[]}
+    
+    for file in files:
+        file_name = file['filename']
+        file_type = file['type']
+        file_url = file['url']
+        
+        ########## REPLACE THIS SECTION WITH OWN RUN CODE #################
+        acceptable_types = {'application/vnd.ms-excel',
+                            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'}
+        
+        #could change what appears in the useful_types based on the file content
+        useful_types = {}
+        
+        file_type_acceptable = file_type in acceptable_types
+        file_type_useable = file_type in useful_types
+        ################## END SECTION ####################################
+        
+        if file_type_acceptable:
+            useableness = 2
+        # elif file_type_useable:
+        #     useableness = 1
+        else:
+            useableness = 0
+        
+        eval_response_manifest["manifest"].append({
+            "filename": file_name,
+            "requirement": useableness})
+       
+    return jsonify(eval_response_manifest)
+
+
+@app.route("/run", methods=["POST"])
+def run():
+    cwd = os.getcwd()
+    
+    zip_path_in = os.path.join(cwd, "To_zip")
+    zip_path_out = os.path.join(cwd, "Zip")
+    
+    #remove to zip directory if it exists
+    try:
+        shutil.rmtree(zip_path_in, ignore_errors=True)
+    except:
+        print("No To_zip exists currently")
+        
+    #make to zip directory
+    os.makedirs(zip_path_in)
+    
+    #take in run manifest
+    run_manifest = request.get_json(force=True)
+    files = run_manifest['manifest']['files']
+    
+    #Read in template to compare to
+    file_path = os.path.join(cwd, "templates", "darpa_template_blank.xlsx")
+    
+    #initiate response manifest
+    run_response_manifest = {"results":[]}
+    
+    
+    for file in files:
+        try:
+            file_name = file['filename']
+            file_type = file['type']
+            file_url = file['url']
+            data = str(file)
+           
+            converted_file_name = f"{file_name}.converted"
+            file_path_out = os.path.join(zip_path_in, converted_file_name)
+        
+            ########## REPLACE THIS SECTION WITH OWN RUN CODE #################
+            #Create own xml files using Excel.py etc.            
+            start_row = 13
+            nrows = 8
+            description_row = 9
+            
+            filled_library, filled_library_metadata, filled_description = read_library(file_url, 
+                start_row = start_row, nrows = nrows, description_row = description_row)
+            
+            blank_library, blank_library_metadata, blank_description = read_library(file_path,  
+                start_row = start_row, nrows = nrows, description_row = description_row)
+            
+            
+            quality_check(filled_library, blank_library, filled_library_metadata, 
+                          blank_library_metadata, filled_description, blank_description)
+            
+            
+            doc = write_sbol(filled_library, filled_library_metadata, filled_description)
+            
+            doc.write(file_path_out)
+            ################## END SECTION ####################################
+            
+            # add name of converted file to manifest
+            run_response_manifest["results"].append({"filename":converted_file_name,
+                                        "sources":[file_name]})
+            
+        except Exception as e:
+            print(e)
+            abort(415)
+            
+    #create manifest file
+    file_path_out = os.path.join(zip_path_in, "manifest.json")
+    with open(file_path_out, 'w') as manifest_file:
+            manifest_file.write(str(run_response_manifest)) 
+        
+    #create zip file of converted files and manifest
+    shutil.make_archive(zip_path_out, 'zip', zip_path_in)
+    
+    #clear To_zip directory
+    shutil.rmtree(zip_path_in, ignore_errors=True)
+    
+    return send_file(f"{zip_path_out}.zip")
+    
+    # #delete zip file
+    # os.remove(f"{zip_path_out}.zip")
